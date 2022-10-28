f82bc2de58fd6078dd51b07a5270d7fffd2ee37e
Aryan <aryaninmail@gmail.com>
2022-10-28 12:22:14 +0530

    initial commit

---
+++ Tic Tac Toe/script.js
@@ -0,0 +1,57 @@
+console.log('welcome to tic tac toe')
+let music = new Audio("music.mp3");
+let audioturn = new Audio("ting.mp3");
+let audiogameover = new Audio("gameover.mp3");
+let turn = "X";
+let gameover = false;
+
+// Function to change the turn 
+const changeTurn = () => {
+    return turn === "X" ? "0" : "X";
+}
+
+// Function to check for a win 
+const checkWin = () => {
+    let boxtexts = document.getElementsByClassName('boxtext');
+    let wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]];
+
+    wins.forEach(e => {
+        if ((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[1]].innerText === boxtexts[e[2]].innerText) && (boxtexts[e[0]].innerText !== "")) {
+            document.querySelector('.info').innerText = boxtexts[e[0]].innerText + " Won";
+            gameover = true;
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px"
+        }
+    })
+
+}
+
+// Game logic
+// music.play();
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', () => {
+        if (boxtext.innerText == '') {
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            audioturn.play();
+            checkWin();
+            if (!gameover)
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+        }
+    })
+})
+
+// Add onclick listener to reset button
+reset.addEventListener('click', () => {
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = "";
+    });
+    turn = "X";
+    gameover = false;
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
+})
